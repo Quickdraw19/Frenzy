c73cf93e472d63ca67b5ce08baaf38aef2e4ae3a
Larry DeGraw <larrydegraw@gmail.com>
2022-06-09 00:18:34 -0600

    Adding first files.

---
+++ matchup.js
@@ -0,0 +1,7 @@
+export default class Matchup {
+    constructor(player1, player2, game) {
+        this.firstPlayer = player1
+        this.secondPlayer = player2
+        this.matchGame = game
+    }
+}
